fd312a77b2a593f3f68b1972149b7ccc6d6e8223
Ashay Humane <ahumane@google.com>
2018-05-09 09:08:47 -0700

    Change source repo.

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ data "google_compute_address" "default" {
 }
 
 module "nat-gateway" {
-  source             = "github.com/GoogleCloudPlatform/terraform-google-managed-instance-group"
+  source             = "github.com/ashayh/terraform-google-managed-instance-group"
   project            = "${var.project}"
   region             = "${var.region}"
   zone               = "${var.zone == "" ? lookup(var.region_params["${var.region}"], "zone") : var.zone}"
