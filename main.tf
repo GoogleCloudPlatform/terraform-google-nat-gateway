2d464fe0f343952f824c8c51e084ba1f1643260e
Dan Isla <disla@google.com>
2018-05-24 09:55:01 -0700

    persist net.ipv4.ip_forward=1 in /etc/sysctl.conf

--- main.tf
+++ main.tf
@@ -20,6 +20,10 @@ data "template_file" "nat-startup-script" {
 
 # Enable ip forwarding and nat
 sysctl -w net.ipv4.ip_forward=1
+
+# Make forwarding persistent.
+sed -i= 's/^[# ]*net.ipv4.ip_forward=[[:digit:]]/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 apt-get update
