c810fc137fa6967d9e3227ea6ddfe6064c3dbd5e
evaillant <emmanuel.v@me.com>
2018-06-04 13:09:18 +0200

    Use a file instead of inline content for script
    This is clearer and more convenient to view, edit & maintain.

--- main.tf
+++ main.tf
@@ -15,34 +15,13 @@
  */
 
 data "template_file" "nat-startup-script" {
-  template = <<EOF
-#!/bin/bash -xe
+  template = "${file("${format("%s/config/startup.sh", path.module)}")}"
 
-# Enable ip forwarding and nat
-sysctl -w net.ipv4.ip_forward=1
-
-# Make forwarding persistent.
-sed -i= 's/^[# ]*net.ipv4.ip_forward=[[:digit:]]/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-
-apt-get update
-
-# Install nginx for instance http health check
-apt-get install -y nginx
-
-ENABLE_SQUID="${var.squid_enabled}"
-
-if [[ "$ENABLE_SQUID" == "true" ]]; then
-  apt-get install -y squid3
-
-  cat - > /etc/squid3/squid.conf <<'EOM'
-${file("${var.squid_config == "" ? "${format("%s/config/squid.conf", path.module)}" : var.squid_config}")}
-EOM
-
-  systemctl reload squid3
-fi
-EOF
+  vars {
+    squid_enabled = "${var.squid_enabled}"
+    squid_config  = "${var.squid_config}"
+    module_path   = "${path.module}"
+  }
 }
 
 data "google_compute_network" "network" {
