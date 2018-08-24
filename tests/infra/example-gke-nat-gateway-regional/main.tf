1f3ef5b1052184af6ab54181bb616d56a9f6ba27
Dan Isla <disla@google.com>
2018-08-24 20:25:11 +0000

    [tests] add get_server_config_beta.sh back into regional example

--- tests/infra/example-gke-nat-gateway-regional/main.tf
+++ tests/infra/example-gke-nat-gateway-regional/main.tf
@@ -32,9 +32,9 @@ provider google {
   region = "${var.region}"
 }
 
-data "google_container_engine_versions" "default" {
-  zone = "${var.zone}"
-}
+# data "google_container_engine_versions" "default" {
+#   zone = "${var.zone}"
+# }
 
 resource "google_compute_network" "tf-ci" {
   name                    = "${var.network_name}"
@@ -53,7 +53,8 @@ resource "google_container_cluster" "tf-ci" {
   name               = "${var.network_name}"
   region             = "${var.region}"
   initial_node_count = 1
-  min_master_version = "${data.google_container_engine_versions.default.latest_node_version}"
+  # min_master_version = "${data.google_container_engine_versions.default.latest_node_version}"
+  min_master_version = "${data.external.container-regional-versions-beta.result.latest_master_version}"
   network            = "${google_compute_subnetwork.tf-ci.network}"
   subnetwork         = "${google_compute_subnetwork.tf-ci.name}"
 
---
+++ tests/infra/example-gke-nat-gateway-regional/regional-versions.tf
@@ -0,0 +1,8 @@
+// External data source to fetch latest regional versions (beta).
+// Use this until this issue is resolved: https://github.com/terraform-providers/terraform-provider-google/issues/1937
+data "external" "container-regional-versions-beta" {
+  program = ["${path.module}/get_server_config_beta.sh"]
+   query = {
+    region = "${var.region}"
+  }
+}
