8be438f45bf10aa9d968c545b24bfd1175f1adf4
Joshua Banton <bantonj@gmail.com>
2018-09-24 11:55:40 -0400

    Changes compute_image from debian-cloud/debian-9 to projects/debian-cloud/global/images/family/debian-9 to prevent Terraform from constantly wanting to rebuild the nat instances.

--- variables.tf
+++ variables.tf
@@ -76,7 +76,7 @@ variable machine_type {
 
 variable compute_image {
   description = "Image used for NAT compute VMs."
-  default     = "debian-cloud/debian-9"
+  default     = "projects/debian-cloud/global/images/family/debian-9"
 }
 
 variable ip {
