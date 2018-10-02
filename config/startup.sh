e79eb9ab1c6793ee8a07771c1d286e8ba37a91c6
Alex Merenda <alex.merenda@impact.com>
2018-10-02 17:08:26 -0400

    adds persistent forward ip

--- config/startup.sh
+++ config/startup.sh
@@ -4,6 +4,7 @@
 sysctl -w net.ipv4.ip_forward=1
 
 # Make forwarding persistent.
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sed -i= 's/^[# ]*net.ipv4.ip_forward=[[:digit:]]/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
